29d6783111bd095f8413ae167e1e6f8d3f2db768
henrique parola <parolajc@hotmail.com>
2023-05-24 18:31:19 +0100

    feature: add base server code

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate_music_route')
+def generate_music():
+    print("cheguei!")
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
